cffa0bc5c4771219b03afed6687982e38a8a5c5c
uchida07 <97789142+uchida07@users.noreply.github.com>
2022-07-30 16:31:05 +0900

    Add files via upload

---
+++ java/Capter05.java
@@ -0,0 +1,5 @@
+public class Capter05 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ java/Chapter04.java
@@ -0,0 +1,12 @@
+public class Chapter04 {
+    public static void main(String[] args) {
+       String str1;
+       int num1;
+       str1 = "UchidaShunsukedayo";
+       num1 = 7;
+       System.out.println(str1);
+       System.out.println(num1);
+       str1 = "aiueo";
+       System.out.println(str1);
+    }
+}

bfa0d55ea39e84ffe4732754fb6058c94398e198
uchida07 <97789142+uchida07@users.noreply.github.com>
2022-08-01 22:02:07 +0900

    Chapter9

---
+++ java/Chapter08.java
@@ -0,0 +1,9 @@
+public class Chapter08 {
+    public static void main(String[] args) {
+       int number = 1;
+        do {
+            number *= 2;
+            System.out.println("DoWhile01 = " + number);
+        } while (number < 50); 
+    }
+}
